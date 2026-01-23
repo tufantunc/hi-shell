759ade9b72b51d5c01c2c38f5b796b79de7ddc57
Tufan Tunç <tufan.tunc.91@gmail.com>
2026-01-23 21:19:46 +0300

    Initial clap integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "hi-shell")]
+#[command(version = "0.1.0")]
+#[command(
+    about = "An intelligent terminal assistant that translates your natural language descriptions into executable bash commands."
+)]
+struct Args {
+    #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
+    input: Vec<String>,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.input.is_empty() {
+        println!("Hi! This is Interactive mode. What do you want to do? (exit to quit)");
+        // we will add REPL loop here
+    } else {
+        let user_input = args.input.join(" ");
+        println!("Input: '{}'", user_input);
+        // We will call llm here
+        println!("(Command will be generated here)");
+    }
 }
